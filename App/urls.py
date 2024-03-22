af8639de7d8c7993c907748c9ccfca50edfecec4
TwoThousand-Four <ykzjere@gmail.com>
2024-03-22 15:02:08 -0300

    contact and aboutUs view added

--- App/urls.py
+++ App/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, contact, aboutUs
+
 urlpatterns = [
     path('', home, name='home'),
+    path('contacto/', contact, name='contact'),
+    path('nosotros/', aboutUs, name='aboutUs'),
+    
 ]
--- App/views.py
+++ App/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home (request):
     return render(request, 'app/home.html') 
+
+def contact (request):
+    return render(request, 'app/contact.html')
+
+def aboutUs (request):
+    return render(request, 'app/aboutUs.html')
