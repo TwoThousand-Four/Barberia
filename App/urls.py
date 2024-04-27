d1aff5a4b925d6e269b58c741e02d9b35ca9583e
TwoThousand-Four <ykzjere@gmail.com>
2024-04-27 16:34:57 -0400

    Inicio de sesion implementado

--- App/urls.py
+++ App/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, contact, services, cart, login
+from .views import home, contact, services, cart, login, register
 
 #Para subir imagenes 
 from django.conf import settings
@@ -11,5 +11,6 @@ urlpatterns = [
     path('servicios/', services, name='services'),
     path('carrito/', cart, name='cart'),
     path('login/', login, name='login'),
+    path('registro/', register, name='register'),
     
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
