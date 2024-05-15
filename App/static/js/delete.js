86a57e55b257a64708299da9686c94fd44741273
TwoThousand-Four <ykzjere@gmail.com>
2024-05-15 17:12:46 -0400

    delete.js editado  y list.html editado

--- App/static/js/delete.js
+++ App/static/js/delete.js
@@ -1,16 +1,15 @@
 function delete_service(id){
     Swal.fire({
-        "title": "¿Estás seguro?",
-        "text": "Esta acción no se puede deshacer",
-        "icon": "question",
-        "showCancelButton":true,
-        "cancelButtonText":"No, cancelar",
-        "confirmButtonText":"Sí, eliminar",
-        "reverseButtons":true,
-        "confirmButtonColor": '#EA0909'
-    }).then(function(result){
+        title: "¿Estás seguro de eliminar este servicio?",
+        text: "Este cambio no se puede revertir!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminarlo!"
+      }).then(function(result){
         if(result.isConfirmed){
             window.location.href="/eliminar/"+id+"/"
         }
-    })
+    });
 }
